1710501e791f910a346ef61c4254e45ef9e001d5
Daniel Motta <danielmotta04@hotmail.com>
2024-06-08 17:23:01 -0300

    feature: adiciona componente de busca

---
+++ github-finder/src/components/Search.tsx
@@ -0,0 +1,46 @@
+import { useRef } from 'react'
+
+import { BsSearch } from 'react-icons/bs'
+
+import styles from './Search.module.css'
+
+type SearchProps = {
+  loadUser: (userName: string) => Promise<void>
+}
+
+const Search = ({ loadUser }: SearchProps) => {
+  const userNameRef = useRef<HTMLInputElement>(null)
+
+  const handleSearch = () => {
+    if (userNameRef.current) {
+      loadUser(userNameRef.current.value)
+    }
+  }
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter' && userNameRef.current) {
+      loadUser(userNameRef.current.value)
+    }
+  }
+
+  return (
+    <div className={styles.search_container}>
+      <h2>Buscar usuário</h2>
+      <div className={styles.search_bar}>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Digite o nome do usuário"
+          ref={userNameRef}
+          onKeyDown={handleKeyDown}
+        />
+        <button onClick={handleSearch}>
+          <BsSearch />
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Search
