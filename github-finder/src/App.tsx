b53d8527762a913351ba736f11916489e004c00f
Daniel Motta <danielmotta04@hotmail.com>
2024-06-08 18:21:46 -0300

    refactor: importa novos componentes e adiciona fluxo de erro

--- github-finder/src/App.tsx
+++ github-finder/src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 
 import Search from './components/Search'
+import Users from './components/Users'
+import Error from './components/Error'
 
 import { UserProps } from './types/user'
 
@@ -8,9 +10,19 @@ import styles from './App.module.css'
 
 function App() {
   const [user, setUser] = useState<UserProps | null>(null)
+  const [error, setError] = useState(false)
 
   const loadUser = async (userName: string) => {
+    setError(false)
+    setUser(null)
+
     const res = await fetch(`https://api.github.com/users/${userName}`)
+
+    if (res.status === 404) {
+      setError(true)
+      return
+    }
+
     const data = await res.json()
 
     const { avatar_url, login, location, followers, following } = data
@@ -30,7 +42,8 @@ function App() {
     <div className={styles.app_container}>
       <h1>Github Finder</h1>
       <Search loadUser={loadUser} />
-      {user && <div>{user.login}</div>}
+      {user && <Users {...user} />}
+      {error && <Error />}
     </div>
   )
 }
