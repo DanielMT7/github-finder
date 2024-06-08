5821346b668a6f14c38b5aa3779c6ca4ccb09268
Daniel Motta <danielmotta04@hotmail.com>
2024-06-08 16:41:55 -0300

    feature: adiciona função que faz o request das informações

--- github-finder/src/App.tsx
+++ github-finder/src/App.tsx
@@ -1,9 +1,23 @@
+import { useState } from 'react'
+
+import { UserProps } from './types/user'
+
 import './App.css'
 
 function App() {
+  const [user, setUser] = useState<UserProps | null>(null)
+
+  const loadUser = async (userName: string) => {
+    const res = await fetch(`https://api.github.com/users/${userName}`)
+    const data = await res.json()
+
+    console.log(data)
+  }
 
   return (
-    <h1>Hello World</h1>
+    <div>
+      <h1>Hello World</h1>
+    </div>
   )
 }
 
